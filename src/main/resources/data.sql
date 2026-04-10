ff90340d6e72a114dd79d9bef81be7e40917d140
xiexyo <2272489099@qq.com>
2026-04-10 14:28:07 +0800

    修改

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,5 +16,12 @@ MERGE INTO t_product (
     product_code, product_name, product_type, currency, sale_status, account_level, created_time, updated_time
 ) KEY(product_code)
 VALUES (
+    'SAV003', '个人人民币三类活期', 'CURRENT', 'CNY', 'ON_SALE', 'CLASS_III', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+);
+
+MERGE INTO t_product (
+    product_code, product_name, product_type, currency, sale_status, account_level, created_time, updated_time
+) KEY(product_code)
+VALUES (
     'SAV003', '个人美元活期-停售示例', 'CURRENT', 'USD', 'OFF_SALE', 'CLASS_II', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
 );
