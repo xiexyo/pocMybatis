9453f61f702121e817b549489865999e13b30468
xiexyo <2272489099@qq.com>
2026-04-10 10:15:25 +0800

    修改

--- src/main/java/com/bank/poc/RetailBankPocApplication.java
+++ src/main/java/com/bank/poc/RetailBankPocApplication.java
@@ -1,14 +1,29 @@
 package com.bank.poc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.bank.poc.mapper")
 public class RetailBankPocApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RetailBankPocApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RetailBankPocApplication.class, args);
+
+        String port = context.getEnvironment().getProperty("server.port", "8080");
+        String applicationName = context.getEnvironment().getProperty("spring.application.name", "application");
+
+        log.info("==============================================");
+        log.info("应用启动成功");
+        log.info("应用名称: {}", applicationName);
+        log.info("本地访问: http://localhost:{}", port);
+        log.info("健康检查: http://localhost:{}/health", port);
+        log.info("Swagger : http://localhost:{}/swagger-ui.html", port);
+        log.info("H2控制台: http://localhost:{}/h2-console", port);
+        log.info("==============================================");
     }
 }
